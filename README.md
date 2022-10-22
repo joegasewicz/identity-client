980081f174a78c6477dbd46aeae7dcbbe0ad9f09
joegasewicz <joegoasewicz@gmail.com>
2022-10-22 16:08:50 +0100

    add a POST request struct method #5 Use token instead of cookie option #6

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
 # Identity Client
 Agnostic identity client
 
-GET method client for remote api calls that takes a JWT token from
-a cookie & includes this token in the headers, whilst trying to be as agnostic with
+HTTP client for remote api calls that takes a JWT token from
+either a cookie or a token value & includes this token in the headers, whilst trying to be as agnostic with
 the returned data type as possible.
 ## Install
 ```
 go get -u github.com/joegasewicz/identity-client
 ```
-
-### Usage
+### Setup
 ```go
 identity := Identity{
 	URL: "http://127.0.01:5000/users",
-	CookieName: "token", // set to "" if a cookie is not required
-    Client &http.Client{} // Optional - useful for testing
+	CookieName: "token", // Optional
+	// If you do not have cookie & require authorisation then set the token value manually
+	Token: "Bearer ..."
+	Client &http.Client{} // Optional (for testing your client)
 }
-data, err := identity.Get(r)
-// cast the data to your required entity type e.g.
-user := data.(*UserModel)
 ```
+### Methods
+#### Get
+```go
+data, err := identity.Get(r)
+```
+#### Post
+```go
+jsonData := map[string]interface{}{"name": "John"}
+data, err := identity.Post(r, jsonData)
+```
+
 The data returned will be of the following type
 ```go
 map[string]interface{}
@@ -29,4 +38,17 @@ You can cast each value to the expected type, for example
 ```go
 d := data.(map[string]interface{})
 var email := d["email"].(string)
+```
+
+
+### Utils
+#### AddTokenPrefix
+When you add a token value to the `Identity` type, you might want to add
+a `Bearer` prefix to the token value (or if you are unsure whether the incoming 
+request token has a Bearer prefix)
+```go
+i := identity_client.Identity{
+    URL:   "http://127.0.01:5000/users",
+    Token: identity_client.AddTokenPrefix(r.Header.Get("Authorization")),
+}
 ```
--- identity-client_test.go
+++ identity-client_test.go
@@ -2,6 +2,7 @@ package identity_client
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -68,6 +69,117 @@ func TestGetReturnsData(t *testing.T) {
 
 	result := data.(map[string]interface{})
 	if result["data"].(float64) != 123 {
+		t.Logf("expected data to equal 123 but got %s", result)
+		t.Fail()
+	}
+}
+
+func TestPostReturnsErrorForNotCookie(t *testing.T) {
+
+	json := `{}`
+
+	GetDoFunc = func(req *http.Request) (*http.Response, error) {
+		r := io.NopCloser(bytes.NewReader([]byte(json)))
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	i := Identity{
+		URL:        "",
+		CookieName: "token",
+		Client:     &MockClient{},
+	}
+
+	req := httptest.NewRequest("GET", "http://domain.com", nil)
+	jsonData := map[string]interface{}{
+		"name": "John",
+	}
+	_, err := i.Post(req, jsonData)
+	expectedErr := "http: named cookie not present"
+	if err.Error() != expectedErr {
+		t.Logf("expected error to be %s but got %s", expectedErr, err.Error())
+		t.Fail()
+	}
+
+}
+
+func TestPostReturnsData(t *testing.T) {
+
+	json := `{"data": 123}`
+
+	GetDoFunc = func(req *http.Request) (*http.Response, error) {
+		r := io.NopCloser(bytes.NewReader([]byte(json)))
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	i := Identity{
+		URL:        "",
+		CookieName: "",
+		Client:     &MockClient{},
+	}
+	req := httptest.NewRequest("GET", "http://domain.com", nil)
+	req.Header.Set("token", "<VALUE>")
+
+	jsonData := map[string]interface{}{
+		"name": "John",
+	}
+	data, err := i.Post(req, jsonData)
+	if err != nil {
+		t.Logf("expected error to be nil but got %e", err)
+		t.Fail()
+	}
+	if data == nil {
+		t.Log("Expected data but got nil")
+		t.Fail()
+	}
+
+	result := data.(map[string]interface{})
+	if result["data"].(float64) != 123 {
+		t.Logf("expected data to equal 123 but got %s", result)
+		t.Fail()
+	}
+}
+
+func TestPostWithToken(t *testing.T) {
+	testToken := "Bearer <TOKEN>"
+	GetDoFunc = func(req *http.Request) (*http.Response, error) {
+		json := fmt.Sprintf(`{"token": "%s"}`, req.Header.Get("Authorization"))
+		r := io.NopCloser(bytes.NewReader([]byte(json)))
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	i := Identity{
+		URL:    "",
+		Token:  testToken,
+		Client: &MockClient{},
+	}
+	req := httptest.NewRequest("GET", "http://domain.com", nil)
+	req.Header.Set("token", "<VALUE>")
+
+	jsonData := map[string]interface{}{
+		"name": "John",
+	}
+	data, err := i.Post(req, jsonData)
+	if err != nil {
+		t.Logf("expected error to be nil but got %e", err)
+		t.Fail()
+	}
+	if data == nil {
+		t.Log("Expected data but got nil")
+		t.Fail()
+	}
+
+	result := data.(map[string]interface{})
+	if result["token"].(string) != testToken {
 		t.Logf("expected data to equal 123 but got %s", result)
 		t.Fail()
 	}
